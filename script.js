721343c9f12717df06a4835a854466657510c267
julioallday <naruto481@gmail.com>
2022-11-18 01:12:46 -0300

    Refatoração

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ let totalPorPessoa;
 let el_gorjetaPessoa;
 let el_botaoPorcentagem;
 let usuarioClicou = false;
-let el_porcentagemCustom;
+let el_botaoPorcentagemCustom;
+let resultado;
+let resultadoGorjeta;
 
 let propBotao = [
   {
@@ -28,63 +30,77 @@ let propBotao = [
       "texto-cinza-claro fundo-cinza-claro fonte-input altura-botoes col-5 btn py-0 py-0 btn-primary px-0 my-2",
   },
 ];
-let botoes = [
-  {
-    nome: "5",
-    id: propBotao[0].id,
-    type: propBotao[0].type,
-    class: propBotao[0].class,
-  },
-  {
-    nome: "10",
-    id: propBotao[0].id,
-    type: propBotao[0].type,
-    class: propBotao[0].class,
-  },
-  {
-    nome: "15",
-    id: propBotao[0].id,
-    type: propBotao[0].type,
-    class: propBotao[0].class,
-  },
-  {
-    nome: "25",
-    id: propBotao[0].id,
-    type: propBotao[0].type,
-    class: propBotao[0].class,
-  },
-  {
-    nome: "50",
-    id: propBotao[0].id,
-    type: propBotao[0].type,
-    class: propBotao[0].class,
-  },
-  {
-    nome: "Custom",
-    id: propBotao[1].id,
-    type: propBotao[1].type,
-    class: propBotao[1].class,
-  },
-];
+async function mapBotoesList() {
+  const botoes = [
+    {
+      nome: "5",
+      id: propBotao[0].id,
+      type: propBotao[0].type,
+      class: propBotao[0].class,
+    },
+    {
+      nome: "10",
+      id: propBotao[0].id,
+      type: propBotao[0].type,
+      class: propBotao[0].class,
+    },
+    {
+      nome: "15",
+      id: propBotao[0].id,
+      type: propBotao[0].type,
+      class: propBotao[0].class,
+    },
+    {
+      nome: "25",
+      id: propBotao[0].id,
+      type: propBotao[0].type,
+      class: propBotao[0].class,
+    },
+    {
+      nome: "50",
+      id: propBotao[0].id,
+      type: propBotao[0].type,
+      class: propBotao[0].class,
+    },
+    {
+      nome: "Custom",
+      id: propBotao[1].id,
+      type: propBotao[1].type,
+      class: propBotao[1].class,
+    },
+  ];
+  return botoes;
+}
 
-mostraBotao(botoes);
-
-function mostraBotao(array) {
+async function mostraBotao(array) {
   array.map((element, index) => {
     if (index < 5) {
-      let el_botaoPorcentagem = document.createElement("button");
       let el_divBotoes = document.getElementById("botoes");
+      let el_botaoPorcentagem = document.createElement("button");
       let dados = array[index].nome;
       el_divBotoes.appendChild(el_botaoPorcentagem);
-      el_botaoPorcentagem.setAttribute("id", array[index].name);
-      el_botaoPorcentagem.setAttribute("type", array[index].type);
+      el_botaoPorcentagem.id = array[index].name;
+      el_botaoPorcentagem.type = array[index].type;
       el_botaoPorcentagem.setAttribute("class", array[index].class);
       el_botaoPorcentagem.setAttribute(
         "onclick",
         "atualizaDados(" + dados + ")"
       );
       el_botaoPorcentagem.innerText = array[index].nome + "%";
-    } else botaoCustom();
+    } else {
+      let props = propBotao[1];
+      let nome = array[index].nome;
+      let el_botaoPorcentagem = document.createElement("button");
+      let el_divBotoes = document.getElementById("botoes");
+      el_divBotoes.appendChild(el_botaoPorcentagem);
+      el_botaoPorcentagem.id = props.id;
+      el_botaoPorcentagem.type = props.type;
+      el_botaoPorcentagem.setAttribute("class", props.class);
+      el_botaoPorcentagem.setAttribute("dir", "rtl");
+      el_botaoPorcentagem.setAttribute("value", "%");
+      el_botaoPorcentagem.setAttribute("onclick", "transformaBotao()");
+      el_botaoPorcentagem.innerText = nome;
+    }
   });
 }
 
@@ -101,19 +117,6 @@ function lidarClick() {
   return usuarioClicou;
 }
 
-function botaoCustom() {
-  let props = propBotao[1];
-  let nome = botoes[5].nome;
-  let el_botaoPorcentagem = document.createElement("button");
-  let el_divBotoes = document.getElementById("botoes");
-  el_divBotoes.appendChild(el_botaoPorcentagem);
-  el_botaoPorcentagem.setAttribute("id", props.id);
-  el_botaoPorcentagem.setAttribute("type", props.type);
-  el_botaoPorcentagem.setAttribute("class", props.class);
-  el_botaoPorcentagem.setAttribute("onclick", "transformaBotao()");
-  el_botaoPorcentagem.innerText = nome;
-}
-
 function transformaBotao() {
   lidarClick();
   if (usuarioClicou) {
@@ -123,8 +126,8 @@ function transformaBotao() {
     el_input.setAttribute("type", "number");
     el_input.setAttribute("id", propBotao[2].id);
     el_input.setAttribute("class", propBotao[2].class);
-    el_porcentagemCustom = document.getElementById(propBotao[2].id);
-    el_porcentagemCustom = el_input.setAttribute(
+    el_botaoPorcentagemCustom = document.getElementById(propBotao[2].id);
+    el_botaoPorcentagemCustom = el_input.setAttribute(
       "oninput",
       "atualizaDadosCustom()"
     );
@@ -148,17 +151,17 @@ function calculaGorjetas(percent) {
   el_total_geral = document.getElementById("total-geral");
   el_gorjetaPessoa = document.getElementById("gorjeta-pessoa");
 
-  let totalPuro = parseInt(el_bill.value) / parseInt(el_pessoas.value);
-  let isANumber = totalPuro > 0;
+  let totalPuro = parseInt(el_bill.value);
   let gorjeta = totalPuro * (parseInt(percent) / 100);
   let total_geral = totalPuro + gorjeta;
+  let isANumber = typeof total_geral;
 
   totalPorPessoa = total_geral / el_pessoas.value;
-  if (isANumber) {
+  if (isANumber == "number") {
     mostraValores(el_total_geral, totalPorPessoa);
     mostraValores(el_gorjetaPessoa, gorjeta);
-    //   el_total_geral.innerText = totalPorPessoa.toFixed(2);
-    //   el_gorjetaPessoa.innerText = gorjeta.toFixed(2);
+    resultadoGorjeta = gorjeta;
+    resultado = totalPorPessoa;
   }
 }
 
@@ -172,9 +175,9 @@ function calculaGorjetasCustom() {
   el_total_geral = document.getElementById("total-geral");
   el_gorjetaPessoa = document.getElementById("gorjeta-pessoa");
 
-  let totalPuro = parseInt(el_bill.value) / parseInt(el_pessoas.value);
+  let totalPuro = parseInt(el_bill.value);
   let isANumber = totalPuro > 0;
-  let gorjeta = (totalPuro * parseInt(el_porcentagem.value)) / 100;
+  let gorjeta = totalPuro * (parseInt(el_porcentagem.value) / 100);
   let total_geral = totalPuro + gorjeta;
 
   totalPorPessoa = total_geral / el_pessoas.value;
@@ -191,6 +194,13 @@ function mostraValores(valor1, valor2) {
     valor1.innerText = valor2.toFixed(2);
   }
 }
+
 function recarregaPagina() {
   window.location.reload(true);
 }
+
+async function main() {
+  const botoesList = await mapBotoesList();
+  await mostraBotao(botoesList);
+}
+main();
